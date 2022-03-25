b7f57a54d56813b439876e9e639bf35db9c70807
mazurenka <an.drej@mail.ru>
2022-03-25 18:58:33 +0100

    11

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Message from "./Message";
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
