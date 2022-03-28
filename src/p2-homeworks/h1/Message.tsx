c4e6a0f566290f8641a9a67a0fe1fd1eae3d5623
mazurenka <an.drej@mail.ru>
2022-03-28 17:41:02 +0200

    1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import s from './Message.module.css'
 
 type MessagePropsType = {
     avatar: string
@@ -9,14 +10,14 @@ type MessagePropsType = {
 
 function Message(props: MessagePropsType) {
     return (
-        <div>
-            <img src={props.avatar} />
+        <div className={s.message}>
+            <img src={props.avatar} alt={'avatar'} className={s.avatar}/>
 
-            <div>{props.name}</div>
-            <div>{props.message}</div>
-            <div>{props.time}</div>
-
-
+            <div className={s.content}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.text}>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
+            </div>
         </div>
     )
 }
